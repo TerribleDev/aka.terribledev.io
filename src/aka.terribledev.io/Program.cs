828fa69694527608214afc9424d5a8d2fea04f4c
Tommy Parnell <tommy@terribledev.io>
2018-08-17 16:32:54 -0400

    use default host

--- src/aka.terribledev.io/Program.cs
+++ src/aka.terribledev.io/Program.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.Configuration;
+using Microsoft.AspNetCore;
 
 namespace aka.terribledev.io
 {
@@ -13,22 +14,11 @@ namespace aka.terribledev.io
     {
         public static void Main(string[] args)
         {
-            var config = new ConfigurationBuilder()
-                .AddCommandLine(args)
-                .AddEnvironmentVariables(prefix: "ASPNETCORE_")
-                .Build();
-
-            var host = new WebHostBuilder()
-                .UseApplicationInsights()
-                .UseConfiguration(config)
-                .UseKestrel()
-                .UseUrls()
-                .UseContentRoot(Directory.GetCurrentDirectory())
-                .UseIISIntegration()
-                .UseStartup<Startup>()
-                .Build();
-
-            host.Run();
+            CreateWebHostBuilder(args).Build().Run();
         }
+
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .UseStartup<Startup>();
     }
 }
